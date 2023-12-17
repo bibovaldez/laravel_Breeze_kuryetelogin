8653a86deee063ee804faa2ea38026319e55a583
bibovaldez <bibovaldez2002@gmail.com>
2023-12-17 17:46:14 +0800

    login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,47 @@
 <x-guest-layout>
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
-        <!-- First Name -->
+        <!-- First form -->
         <div>
-            <x-input-label for="fname" :value="__('First Name')" />
-            <x-text-input id="fname" class="block mt-1 w-full" type="text" name="fname" :value="old('fname')" required
-                autofocus autocomplete="given-name" />
-            <x-input-error :messages="$errors->get('fname')" class="mt-2" />
+            <!-- First Name -->
+            <div>
+                <x-input-label for="fname" :value="__('First Name')" />
+                <x-text-input id="fname" class="block mt-1 w-full" type="text" name="fname" :value="old('fname')"
+                    required autofocus autocomplete="given-name" />
+                <x-input-error :messages="$errors->get('fname')" class="mt-2" />
+            </div>
+            <!-- Middle Name -->
+            <div class="mt-4">
+                <x-input-label for="mname" :value="__('Middle Name')" />
+                <x-text-input id="mname" class="block mt-1 w-full" type="text" name="mname" :value="old('mname')"
+                    required autofocus autocomplete="additional-name" />
+                <x-input-error :messages="$errors->get('mname')" class="mt-2" />
+            </div>
+            <!-- Last Name -->
+            <div class="mt-4">
+                <x-input-label for="lname" :value="__('Last Name')" />
+                <x-text-input id="lname" class="block mt-1 w-full" type="text" name="lname" :value="old('lname')"
+                    required autofocus autocomplete="family-name" />
+                <x-input-error :messages="$errors->get('lname')" class="mt-2" />
+            </div>
+            <!-- Phone Number -->
+            <div class="mt-4">
+                <x-input-label for="phone" :value="__('Phone Number')" />
+                <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')"
+                    required autofocus autocomplete="tel" />
+                <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+            </div>
         </div>
-        <!-- Middle Name -->
-        <div class="mt-4">
-            <x-input-label for="mname" :value="__('Middle Name')" />
-            <x-text-input id="mname" class="block mt-1 w-full" type="text" name="mname" :value="old('mname')"
-                required autofocus autocomplete="additional-name" />
-            <x-input-error :messages="$errors->get('mname')" class="mt-2" />
-        </div>
-        <!-- Last Name -->
-        <div class="mt-4">
-            <x-input-label for="lname" :value="__('Last Name')" />
-            <x-text-input id="lname" class="block mt-1 w-full" type="text" name="lname" :value="old('lname')"
-                required autofocus autocomplete="family-name" />
-            <x-input-error :messages="$errors->get('lname')" class="mt-2" />
-        </div>
-        <!-- Phone Number -->
-        <div class="mt-4">
-            <x-input-label for="phone" :value="__('Phone Number')" />
-            <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')"
-                required autofocus autocomplete="tel" />
-            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
-        </div>
+
+
 
 
         <!-- Province Dropdown -->
         <div class="mt-4">
             <x-input-label for="phone" :value="__('Province')" />
-            <select id="provinceSelect" name="province" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full">
+            <select id="provinceSelect" name="province"
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full">
                 <option value="" disabled selected>Select Province</option>
             </select>
         </div>
@@ -44,17 +51,17 @@
         <!-- Municipality Dropdown -->
         <div class="mt-4">
             <x-input-label for="phone" :value="__('Municipality')" />
-            <select id="municipalitySelect" name="municipality" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full">
+            <select id="municipalitySelect" name="municipality"
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full">
                 <option value="" disabled selected>Select Municipality</option>
             </select>
         </div>
         <!-- Barangay Dropdown -->
         <div class="mt-4">
             <x-input-label for="phone" :value="__('Barangay')" />
-            <select id="barangaySelect" name="barangay" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full">
-                <option value="" disabled selected
-                class="text-gray-400"
-                >Select Barangay</option>
+            <select id="barangaySelect" name="barangay"
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 w-full">
+                <option value="" disabled selected class="text-gray-400">Select Barangay</option>
             </select>
         </div>
 
@@ -98,6 +105,8 @@
         </div>
 
     </form>
+
+
 
     <script>
         // Fetch provinces and populate dropdown on page load
